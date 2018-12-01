e3ebb43b97f9b8822aa95e6dd84bba9bf715828e
Martin Zackrisson <martin.zackrisson@gmail.com>
2018-12-01 13:44:10 +0100

    Rail

---
+++ Assets/Scripts/Rail.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rail : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    public int X
+    {
+        get
+        {
+            return Mathf.RoundToInt(transform.position.x);
+        }
+    }
+
+    public int Y
+    {
+        get
+        {
+            return Mathf.RoundToInt(transform.position.y);
+        }
+    }
+}
