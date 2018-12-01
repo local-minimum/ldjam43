d50f09df7dbd33ad549360d3e6cbb5da28cdcce1
Martin Zackrisson <martin.zackrisson@gmail.com>
2018-12-01 20:28:44 +0100

    Crash Bang Boom

--- Assets/Scripts/Train.cs
+++ Assets/Scripts/Train.cs
@@ -56,5 +56,15 @@ public class Train : MonoBehaviour {
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log(collision.gameObject.name);
+        var contact = collision.contacts[0];
+        collision.rigidbody.AddForceAtPosition(contact.normal*-50, contact.point + Vector3.up * 0.4f);
+        StartCoroutine(Kill(collision.gameObject));
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
+
+    IEnumerator<WaitForSeconds> Kill(GameObject go)
+    {
+        yield return new WaitForSeconds(2f);
+        GameObject.Destroy(go);
     }
 }
