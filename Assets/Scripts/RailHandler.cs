9fcb5d1d2e2d7d5ac395f1d337bd29c7fc5be858
Jonas Svensson <jonas.m.svensson@gmail.com>
2018-12-01 14:47:21 +0100

    Added more scripts

---
+++ Assets/Scripts/RailHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public Rail FindRailAtCoordinates(long X, long Y)
+    {
+        var rails = FindObjectsOfType<Rail>();
+        for (int i=0;i<rails.Length;++i)
+        {
+            var rail = rails[i];
+            if (rail.X == X && rail.Y == Y)
+            {
+                return rail;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject worldBoundary;
+
+    private Vector3 min;
+    private Vector3 max;
+
+    // Use this for initialization
+    void Start () {
+        var x = worldBoundary.transform.position.x;
+        var y = worldBoundary.transform.position.y;
+        var boundaryRenderer = worldBoundary.GetComponent<Renderer>();
+        max = boundaryRenderer.bounds.max;
+        min = boundaryRenderer.bounds.min;
+    }
+
+    public Vector2 GetRandomPosition() {
+        return new Vector2(Random.Range(min.x, max.x), Random.Range(min.y, max.x));
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
